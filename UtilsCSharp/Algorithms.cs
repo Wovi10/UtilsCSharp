3358b92e72870d2c9300fa2d4e8ba6eaca2d76a0
Wout Vinckevleugel <wout.vinck@gmail.com>
2024-03-28 14:37:44 +0100

    Added Algorithms

---
+++ UtilsCSharp/Algorithms.cs
@@ -0,0 +1,32 @@
+﻿namespace UtilsCSharp;
+
+public class Algorithms
+{
+    #region Floyd's Tortoise and Hare (Cycle Detection)
+
+    public static int GetCycleLength(List<int> list)
+    {
+        var n = list.Count;
+
+        for (var cycleLength = 1; cycleLength <= n / 2; cycleLength++)
+        {
+            var isCycle = true;
+            for (var i = 0; i < n - cycleLength; i++)
+            {
+                if (list[i] == list[i + cycleLength]) 
+                    continue;
+
+                isCycle = false;
+                break;
+            }
+            if (isCycle)
+            {
+                return cycleLength;
+            }
+        }
+
+        return 0; // No cycle found
+    }
+
+    #endregion
+}
