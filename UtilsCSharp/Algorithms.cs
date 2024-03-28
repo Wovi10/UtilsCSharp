78ecdb455c9b2f85fbf795e4597f5244843367c4
Wout Vinckevleugel <wout.vinck@gmail.com>
2024-03-28 15:27:34 +0100

    Update Algorithms.cs

--- UtilsCSharp/Algorithms.cs
+++ UtilsCSharp/Algorithms.cs
@@ -46,6 +46,7 @@ public class Algorithms
             var hasCycle = false;
             for (var i = n - 1; i >= cycleLength; i--)
             {
+                // Check if the last cycleLength elements are the same as the cycleLength elements before that
                 var endOfListRange = list[(n - cycleLength)..n];
                 var endOfListMinusCycleLengthRange = list[(n - cycleLength * 2)..(n - cycleLength)];
 
