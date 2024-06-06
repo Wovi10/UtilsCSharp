f1885d1b7f6b24fbf37c1a8978967edcff4af4a4
Wout Vinckevleugel <wout.vinck@gmail.com>
2024-06-06 07:50:43 +0200

    Update MathUtils.cs

--- UtilsCSharp/MathUtils.cs
+++ UtilsCSharp/MathUtils.cs
@@ -697,21 +697,24 @@ public static class MathUtils
 
     #region IsBetween
 
-    public static bool IsBetween(this int a, int lowerBound, int upperBound, bool leftInclusive = true, bool rightInclusive = true)
-    {
-        var left = leftInclusive ? a >= lowerBound : a > lowerBound;
-        var right = rightInclusive ? a <= upperBound : a < upperBound;
-        return left && right;
-    }
-    
-    public static bool IsBetween(this long a, long lowerBound, long upperBound, bool leftInclusive = true, bool rightInclusive = true)
+    public static bool IsBetween(this int a, int lowerBound, int upperBound, bool leftInclusive = true,
+        bool rightInclusive = true)
     {
         var left = leftInclusive ? a >= lowerBound : a > lowerBound;
         var right = rightInclusive ? a <= upperBound : a < upperBound;
         return left && right;
     }
 
-    public static bool IsBetween(this double a, double lowerBound, double upperBound, bool leftInclusive = true, bool rightInclusive = true)
+    public static bool IsBetween(this long a, long lowerBound, long upperBound, bool leftInclusive = true,
+        bool rightInclusive = true)
+    {
+        var left = leftInclusive ? a >= lowerBound : a > lowerBound;
+        var right = rightInclusive ? a <= upperBound : a < upperBound;
+        return left && right;
+    }
+
+    public static bool IsBetween(this double a, double lowerBound, double upperBound, bool leftInclusive = true,
+        bool rightInclusive = true)
     {
         var left = leftInclusive ? a >= lowerBound : a > lowerBound;
         var right = rightInclusive ? a <= upperBound : a < upperBound;
