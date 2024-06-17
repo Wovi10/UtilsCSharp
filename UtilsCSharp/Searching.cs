824e68632db42690844bca271d9141069a7b0976
Wout Vinckevleugel <wout.vinck@gmail.com>
2024-06-17 10:27:02 +0200

    Added BinarySearch

---
+++ UtilsCSharp/Searching.cs
@@ -0,0 +1,35 @@
+﻿namespace UtilsCSharp;
+
+public static class Searching
+{
+    /// <summary>
+    /// Binary search algorithm
+    /// </summary>
+    /// <param name="input"></param>
+    /// <param name="searchKey"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns>The index at which you can find the item to be found. The length of the input if not found.</returns>
+    public static int BinarySearch<T>(this IEnumerable<T> input, T searchKey) where T : struct, IComparable<T>
+    {
+        var inputArray = input as T[] ?? input.ToArray();
+
+        var lowerBound = 0;
+        var upperBound = inputArray.Length - 1;
+
+        while (true)
+        {
+            var currentIndex = (lowerBound + upperBound) / 2;
+
+            if (inputArray.ElementAt(currentIndex).Equals(searchKey))
+                return currentIndex;
+
+            if (lowerBound > upperBound)
+                return inputArray.Length;
+
+            if (inputArray.ElementAt(currentIndex).IsSmallerThan(searchKey))
+                lowerBound = currentIndex + 1;
+            else
+                upperBound = currentIndex - 1;
+        }
+    }
+}
---
+++ UtilsCSharpTests/SearchingTesting/SearchingTests.cs
@@ -0,0 +1,15 @@
+﻿using UtilsCSharp;
+
+namespace UtilsCSharpTests.SearchingTesting;
+
+public class SearchingTests
+{
+    [Test, TestOf(nameof(Searching.BinarySearch))]
+    [TestCaseSource(typeof(TestData), nameof(TestData.BinarySearchCases))]
+    public void BinarySearchTest<T>(int expected, IEnumerable<T> input, T searchKey)
+        where T : struct, IComparable<T>
+    {
+        var actual = input.BinarySearch(searchKey);
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+}
