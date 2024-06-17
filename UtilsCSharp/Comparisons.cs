99225f67fbbd56a3f0dbfa99700eacda364d7f75
Wout Vinckevleugel <wout.vinck@gmail.com>
2024-06-17 10:03:57 +0200

    Adjusting constraints for T

--- UtilsCSharp/Comparisons.cs
+++ UtilsCSharp/Comparisons.cs
@@ -6,14 +6,14 @@ public static class Comparisons
 {
     #region GetHighest
 
-    public static T GetHighest<T>(T a, T b) where T: struct, INumber<T>
+    public static T GetHighest<T>(T a, T b) where T: struct, IComparable<T>
         => a.CompareTo(b) > 0 ? a : b;
 
     #endregion
     
     #region GetLowest
 
-    public static T GetLowest<T>(T a, T b) where T: struct, INumber<T>
+    public static T GetLowest<T>(T a, T b) where T: struct, IComparable<T>
         => a.CompareTo(b) < 0 ? a : b;
 
     #endregion
@@ -34,7 +34,7 @@ public static class Comparisons
 
     #region IsGreaterThan
 
-    public static bool IsGreaterThan<T>(this T a, T b) where T: struct, INumber<T>
+    public static bool IsGreaterThan<T>(this T a, T b) where T: struct, IComparable<T>
     {
         return a.CompareTo(b) switch
         {
@@ -43,14 +43,14 @@ public static class Comparisons
         };
     }
 
-    public static bool? IsBiggerThan<T>(this T a, T b) where T: struct, INumber<T>
+    public static bool IsBiggerThan<T>(this T a, T b) where T: struct, IComparable<T>
         => a.IsGreaterThan(b);
 
     #endregion
 
     #region IsLessThan
 
-    public static bool IsLessThan<T>(this T a, T b) where T: struct, INumber<T>
+    public static bool IsLessThan<T>(this T a, T b) where T: struct, IComparable<T>
     {
         return a.CompareTo(b) switch
         {
@@ -59,14 +59,14 @@ public static class Comparisons
         };
     }
     
-    public static bool? IsSmallerThan<T>(this T a, T b) where T: struct, INumber<T>
+    public static bool IsSmallerThan<T>(this T a, T b) where T: struct, IComparable<T>
         => a.IsLessThan(b);
 
     #endregion
     
     #region IsGreaterThanOrEqualTo
 
-    public static bool IsGreaterThanOrEqualTo<T>(this T a, T b) where T: struct, INumber<T>
+    public static bool IsGreaterThanOrEqualTo<T>(this T a, T b) where T: struct, IComparable<T>
     {
         return a.CompareTo(b) switch
         {
@@ -80,7 +80,7 @@ public static class Comparisons
 
     #region IsLessThanOrEqualTo
 
-    public static bool IsLessThanOrEqualTo<T>(this T a, T b) where T: struct, INumber<T>
+    public static bool IsLessThanOrEqualTo<T>(this T a, T b) where T: struct, IComparable<T>
     {
         return a.CompareTo(b) switch
         {
@@ -89,16 +89,26 @@ public static class Comparisons
             _ => true
         };
     }
+    
+    public static bool IsSmallerOrEqualTo<T>(this T a, T b) where T: struct, IComparable<T>
+        => a.IsLessThanOrEqualTo(b);
 
     #endregion
 
     #region IsBetween
 
     public static bool IsBetween<T>(this T input, T lowerBound, T upperBound, bool leftInclusive = true,
-        bool rightInclusive = true) where T: struct, INumber<T>
+        bool rightInclusive = true) where T: struct, IComparable<T>
     {
-        var left = leftInclusive ? IsGreaterThanOrEqualTo(input,lowerBound) : IsGreaterThan(input, lowerBound);
-        var right = rightInclusive ? IsLessThanOrEqualTo(input, upperBound) : IsLessThan(input, upperBound);
+        var left = 
+            leftInclusive
+                ? IsGreaterThanOrEqualTo(input,lowerBound)
+                : IsGreaterThan(input, lowerBound);
+
+        var right = 
+            rightInclusive 
+                ? IsLessThanOrEqualTo(input, upperBound)
+                : IsLessThan(input, upperBound);
 
         return left && right;
     }
--- UtilsCSharp/Sorting.cs
+++ UtilsCSharp/Sorting.cs
@@ -1,10 +1,8 @@
-﻿using System.Numerics;
-
-namespace UtilsCSharp;
+﻿namespace UtilsCSharp;
 
 public static class Sorting
 {
-    public static List<T> BubbleSort<T>(List<T> list) where T: struct, INumber<T>
+    public static List<T> BubbleSort<T>(List<T> list) where T : struct, IComparable<T>
     {
         bool swapped;
         do
@@ -15,7 +13,7 @@ public static class Sorting
                 var current = list[i];
                 var next = list[i + 1];
 
-                if (current <= next)
+                if (current.IsSmallerOrEqualTo(next))
                     continue;
 
                 (list[i], list[i + 1]) = (next, current);
@@ -26,33 +24,33 @@ public static class Sorting
         return list;
     }
 
-    public static List<T> SelectionSort<T>(List<T> list) where T : struct, INumber<T>
+    public static List<T> SelectionSort<T>(List<T> list) where T : struct, IComparable<T>
     {
         for (var i = 0; i < list.Count; i++)
         {
             var minimum = i;
-            for (var j = i+1; j < list.Count; j++)
+            for (var j = i + 1; j < list.Count; j++)
             {
-                if (list[j] < list[minimum])
+                if (list[j].IsSmallerThan(list[minimum]))
                     minimum = j;
             }
-            
+
             if (minimum == i)
                 continue;
-            
+
             (list[i], list[minimum]) = (list[minimum], list[i]);
         }
 
         return list;
     }
-    
-    public static List<T> InsertionSort<T>(List<T> list) where T : struct, INumber<T>
+
+    public static List<T> InsertionSort<T>(List<T> list) where T : struct, IComparable<T>
     {
         for (var i = 1; i < list.Count; i++)
         {
             var current = list[i];
             var j = i - 1;
-            while (j >= 0 && list[j] > current)
+            while (j >= 0 && list[j].IsGreaterThan(current))
             {
                 list[j + 1] = list[j];
                 j--;
